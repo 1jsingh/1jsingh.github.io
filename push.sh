106c8c463fba3e33ffb4f4889d77860d5521df7a
undrcvernatforce <1.jaskiratsingh@gmail.com>
2018-06-22 11:11:09 +0900

    update

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
 git add -A && git commit -m "update"
-git push https://undrcvernatforce:DDGTBGWAHEGURU*7@github.com/1jsingh/1jsingh.github.io.git master
+git push https://1jsingh:DDGTBGWAHEGURU*7@github.com/1jsingh/1jsingh.github.io.git master
